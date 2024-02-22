827d862944172f051c1014be7c11d6b512281bfd
Mehmet4602can <mehmetcelik4602@gmail.com>
2024-02-22 10:05:34 +0300

    Proje dosyası ekle.

---
+++ workSafe/Controllers/WorkSafeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace workSafe.Controllers
+{
+    public class WorkSafeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public FileResult jsonResim()
+        {
+            return File("~/imge/isguvenligi.jpg", "image/jpg");
+        }
+        [Route("WorkSafe/gt/{id}")]
+        public string gt(int id)
+        {
+            return $"geri gönder = {id}";
+        }
+    }
+}
---
+++ workSafe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace workSafe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
